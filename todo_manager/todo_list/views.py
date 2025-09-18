ff944c4a9115a0a78637184bc41c088fbcdb2459
Pavel Baranov <p204368@gmail.com>
2025-09-18 18:30:20 +0300

    created class by the ListView

--- todo_manager/todo_list/views.py
+++ todo_manager/todo_list/views.py
@@ -1,3 +1,35 @@
+from django.http import (
+    HttpRequest,
+    HttpResponse,
+)
 from django.shortcuts import render
 
-# Create your views here.
+from .models import ToDoItem
+
+from django.views.generic import TemplateView, ListView, DetailView
+
+
+def index_view(request: HttpRequest) -> HttpResponse:
+    todo_items = ToDoItem.objects.all()[:3]
+    return render(
+        request,
+        template_name="todo_list/index.html",
+        context={"todo_items": todo_items},
+    )
+
+
+class ToDoListIndexView(ListView):
+    template_name = "todo_list/index.html"
+    queryset = ToDoItem.objects.all()
+
+
+class ToDoListView(ListView):
+    model = ToDoItem
+
+
+class ToDoListDoneView(ListView):
+    queryset = ToDoItem.objects.filter(done=True).all()
+
+
+class ToDoDetailView(DetailView):
+    model = ToDoItem
--- todo_manager/todo_manager/urls.py
+++ todo_manager/todo_manager/urls.py
@@ -3,7 +3,7 @@ from django.urls import path, include
 from django.views.generic import TemplateView
 
 urlpatterns = [
-    path("admin/", admin.site.urls),
-    path("todo-list/", include("todo_list.urls")),
     path("", TemplateView.as_view(template_name="index.html"), name='index'),
+    path("admin/", admin.site.urls),
+    path("todos/", include("todo_list.urls")),
 ]
